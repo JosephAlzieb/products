f42a9d4fc6727feee2487a59ce98a1138a2e5516
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-02-21 00:03:46 +0100

    commandlinerunner um zu testen, dass man users in die datenbank speichern kann

--- src/main/java/de/hhu/thymeleafsqlseq/ThymeleafSqlSeqApplication.java
+++ src/main/java/de/hhu/thymeleafsqlseq/ThymeleafSqlSeqApplication.java
@@ -1,14 +1,35 @@
 package de.hhu.thymeleafsqlseq;
 
+import de.hhu.thymeleafsqlseq.domain.User;
+import de.hhu.thymeleafsqlseq.services.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-public class ThymeleafSqlSeqApplication {
+public class ThymeleafSqlSeqApplication implements CommandLineRunner{
+
+    private UserService userService;
+
+    public ThymeleafSqlSeqApplication(UserService userService) {
+        this.userService = userService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ThymeleafSqlSeqApplication.class, args);
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user1 = new User("joe1", "az", "joe1.hhu.de", "1122", "user");
+        User user2 = new User("joe2", "az", "joe2.hhu.de", "1122", "admin");
+        User user3 = new User("joe3", "az", "joe3.hhu.de", "1122", "user");
+        userService.saveUser(user1);
+        userService.saveUser(user2);
+        userService.saveUser(user3);
+
     }
 
 }
