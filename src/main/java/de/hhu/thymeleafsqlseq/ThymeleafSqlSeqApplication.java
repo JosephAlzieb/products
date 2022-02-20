f9af539944bf1046c0764900dd8660dcd4a572f7
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-02-20 12:39:24 +0100

    initial commit

---
+++ src/main/java/de/hhu/thymeleafsqlseq/ThymeleafSqlSeqApplication.java
@@ -0,0 +1,13 @@
+package de.hhu.thymeleafsqlseq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafSqlSeqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafSqlSeqApplication.class, args);
+    }
+
+}
