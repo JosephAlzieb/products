b148bc07a4d0a9fcc6aa498288eea618ebc3065d
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-02-20 21:19:52 +0100

    folgend Fehlermeldung gelöst : APPLICATION FAILED TO START Consider the following:     If you want an embedded database (H2, HSQL or Derby), please put it on the classpath.     If you have database settings to be loaded from a particular profile you may need to activate it (no profiles are currently active).

--- src/main/java/de/hhu/thymeleafsqlseq/ThymeleafSqlSeqApplication.java
+++ src/main/java/de/hhu/thymeleafsqlseq/ThymeleafSqlSeqApplication.java
@@ -2,8 +2,9 @@ package de.hhu.thymeleafsqlseq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ThymeleafSqlSeqApplication {
 
     public static void main(String[] args) {
