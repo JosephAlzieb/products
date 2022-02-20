e571a56fd216c9f0f7db5f54f231e88989d66bf3
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-02-20 15:25:03 +0100

    UserRepo implementiert

---
+++ src/main/java/de/hhu/thymeleafsqlseq/db/UserRepoImpl.java
@@ -0,0 +1,28 @@
+package de.hhu.thymeleafsqlseq.db;
+
+import de.hhu.thymeleafsqlseq.domain.Role;
+import de.hhu.thymeleafsqlseq.domain.User;
+import de.hhu.thymeleafsqlseq.dto.UserDto;
+import de.hhu.thymeleafsqlseq.repositories.UserRepository;
+
+public class UserRepoImpl implements UserRepository {
+
+    private UserRepoDau repoDau;
+
+    public UserRepoImpl(UserRepoDau repoDau) {
+        this.repoDau = repoDau;
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        UserDto dto = repoDau.findUserByEmail(email);
+        User user = new User(dto.firstName(), dto.lastName(), dto.email(),dto.password(), Role.valueOf(dto.role()));
+        return user;
+    }
+
+    @Override
+    public void save(User user) {
+        UserDto userDto = new UserDto(null, user.getFirstName(),user.getLastName(), user.getEmail(),user.getPassword(),user.getActive(), user.getRoles().name());
+        repoDau.save(userDto);
+    }
+}
