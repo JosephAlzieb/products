c92955f6703aa1fe9efb1ddf497fe095fda0229f
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-02-21 20:06:51 +0100

    save methode zu Service hinzugefügt

--- src/main/java/de/hhu/thymeleafsqlseq/services/ProductService.java
+++ src/main/java/de/hhu/thymeleafsqlseq/services/ProductService.java
@@ -3,7 +3,9 @@ package de.hhu.thymeleafsqlseq.services;
 
 import de.hhu.thymeleafsqlseq.domain.Product;
 import de.hhu.thymeleafsqlseq.repositories.ProductRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProductService {
     private ProductRepository repository;
 
@@ -13,5 +15,9 @@ public class ProductService {
 
     public Product getProduct (long id){
         return repository.findProductById(id);
+    }
+
+    public void saveProduct (Product product){
+        repository.saveProduct(product);
     }
 }
