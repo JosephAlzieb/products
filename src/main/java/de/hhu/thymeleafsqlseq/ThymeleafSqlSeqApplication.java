7c23fd9cf5b7c28986c8e767a8d15f4ce08ca290
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-02-21 17:50:57 +0100

    CommandLineRunner nicht mehr nötig. nun kann man Users erstellen und in Datenbank speichern.

--- src/main/java/de/hhu/thymeleafsqlseq/ThymeleafSqlSeqApplication.java
+++ src/main/java/de/hhu/thymeleafsqlseq/ThymeleafSqlSeqApplication.java
@@ -23,13 +23,6 @@ public class ThymeleafSqlSeqApplication implements CommandLineRunner{
 
     @Override
     public void run(String... args) throws Exception {
-        User user1 = new User("joe1", "az", "joe1.hhu.de", "1122", "user");
-        User user2 = new User("joe2", "az", "joe2.hhu.de", "1122", "admin");
-        User user3 = new User("joe3", "az", "joe3.hhu.de", "1122", "user");
-        userService.saveUser(user1);
-        userService.saveUser(user2);
-        userService.saveUser(user3);
-
     }
 
 }
