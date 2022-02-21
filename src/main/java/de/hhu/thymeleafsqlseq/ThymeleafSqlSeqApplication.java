099b62b1467052df50749991de68ab854e036962
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-02-21 17:48:06 +0100

    exclude = {DataSourceAutoConfiguration.class } ist nicht nötig

--- src/main/java/de/hhu/thymeleafsqlseq/ThymeleafSqlSeqApplication.java
+++ src/main/java/de/hhu/thymeleafsqlseq/ThymeleafSqlSeqApplication.java
@@ -5,9 +5,9 @@ import de.hhu.thymeleafsqlseq.services.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class ThymeleafSqlSeqApplication implements CommandLineRunner{
 
     private UserService userService;
