b60f7d19bf89822fd9eff9c4eed590d91e3fbe03
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-02-22 02:34:02 +0100

    der erste acrhunit test.. kein @Controller annotation außerhalb das Package controllers

---
+++ src/test/java/de/hhu/thymeleafsqlseq/archunit/ArchLawsTest.java
@@ -0,0 +1,20 @@
+package de.hhu.thymeleafsqlseq.archunit;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import de.hhu.thymeleafsqlseq.ThymeleafSqlSeqApplication;
+import org.springframework.stereotype.Controller;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+@AnalyzeClasses(packagesOf = ThymeleafSqlSeqApplication.class )
+public class ArchLawsTest {
+
+    @ArchTest
+    static final ArchRule ALL_CONTROLLERS_SHOULD_RESIDE_IN_CONTROLLERS_PACKAGE = classes()
+            .that()
+            .resideOutsideOfPackages("..controllers..")
+            .should()
+            .notBeAnnotatedWith(Controller.class);
+}
