fa221eda846431c318419046f23284dc67fe468e
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-02-23 17:46:59 +0100

    Konfiguration ersetzt, damit man sich erstmal einloggen muss

--- src/main/java/de/hhu/thymeleafsqlseq/security/WebSecurityConfiguration.java
+++ src/main/java/de/hhu/thymeleafsqlseq/security/WebSecurityConfiguration.java
@@ -1,24 +1,40 @@
 package de.hhu.thymeleafsqlseq.security;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Value("${products.rollen.admin}")
+    private Set<String> admins;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(a -> a
                         .antMatchers("/", "/error", "/css/**", "/img/**").permitAll()
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
                 )
                 .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
                 .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
-                .logout(l -> l.logoutSuccessUrl("/").permitAll());
+                .logout(l -> l.logoutSuccessUrl("/").permitAll()).oauth2Login();
     }
 }
