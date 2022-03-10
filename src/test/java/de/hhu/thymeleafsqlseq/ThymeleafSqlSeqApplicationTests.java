b29b301fa2576a77573a0a1e4f02d80f2136cf89
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-03-10 16:12:45 +0100

    Test auskommentiert

--- src/test/java/de/hhu/thymeleafsqlseq/ThymeleafSqlSeqApplicationTests.java
+++ src/test/java/de/hhu/thymeleafsqlseq/ThymeleafSqlSeqApplicationTests.java
@@ -1,13 +1,13 @@
 package de.hhu.thymeleafsqlseq;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ThymeleafSqlSeqApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ThymeleafSqlSeqApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
